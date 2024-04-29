86564bd6c128c732e858a17ba2fcaa2d88f8310e
Vitor Parajara <vitor.v.parajara@gmail.com>
2024-04-29 17:21:58 -0300

    mexendo com js

---
+++ 04_29/js/hello.js
@@ -0,0 +1,15 @@
+alert("Segundou...")
+
+//Meu primeiro comentário
+
+/*Meu segundo comentário*/
+
+// Criando uma variável
+let texto = "Aula Javascript"
+console.log(texto)
+let decimal = 20.8
+console.log(decimal)
+let inteiro = 20
+console.log(inteiro)
+let booleano = true
+console.log(booleano)
